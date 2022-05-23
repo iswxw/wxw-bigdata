c7095320cb45b6631d57a56504bdf76668112af3
weixiaowei <1550518923@qq.com>
2022-05-23 22:39:33 +0800

    add test

---
+++ wxw-hive/src/main/java/com/wxw/HiveApplication.java
@@ -0,0 +1,16 @@
+package com.wxw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @contract: 公众号：Java半颗糖
+ * @desc:
+ * @link:
+ */
+@SpringBootApplication
+public class HiveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HiveApplication.class,args);
+    }
+}
---
+++ wxw-spark/src/main/java/com/wxw/SparkApplication.java
@@ -0,0 +1,17 @@
+package com.wxw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @contract: 公众号：技术能量站
+ * @desc:
+ * @link:
+ * @company:
+ */
+@SpringBootApplication
+public class SparkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SparkApplication.class,args);
+    }
+}
